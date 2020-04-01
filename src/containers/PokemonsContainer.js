7378ffc1e8f2b2cb09571386ca57bd8a91828b3e
Mel <jones.mel@gmail.com>
2020-04-01 15:59:41 +0200

    useQuery & pass data to <Pokemon />

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Pokemon } from '../components/Pokemon'
 
 export function PokemonsContainer() {
+  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+    variables: { first: 9 },
+  })
+
   return (
-    <div>
-      <p>I am a pokemon container</p>
+    <div className="container">
+      {pokemons &&
+        pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
     </div>
   )
 }
