0e158cfa2f883b48c8dc9beaa565708794159ae4
Mel <jones.mel@gmail.com>
2020-04-01 13:36:44 +0200

    adding ApolloClient & ApolloProvider

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { App } from './src/app'
-import './style.scss'
+import { App } from './src/App.js'
+import './src/style.scss'
 
 render(<App />, document.getElementById('root'))
--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+export function App() {
+  const client = new ApolloClient({
+    url: 'https://graphql-pokemon.now.sh',
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonsContainer />
+      </main>
+    </ApolloProvider>
+  )
+}
